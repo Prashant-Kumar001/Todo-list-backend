8495978e06589d25d363acbe9145d4e7869d27ef
Prashant <prashant0kumar101@gmail.com>
2026-06-28 17:22:31 +0530

    fix cors issue added frontend URL

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://todo-list-backend-sand.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
